b9f7c0e95fb1f995e964777a3491fe851ed23800
Garvit Kumar <garvitkajot854@gmail.com>
2025-08-06 15:41:34 +0000

    where i think it is not working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,12 +94,18 @@ export default {
         'glow': {
           '0%, 100%': { opacity: '0.75' },
           '50%': { opacity: '1' },
-        }
+        },
+        "border-beam": {
+          "100%": {
+            "offset-distance": "100%",
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'glow': 'glow 4s ease-in-out infinite'
+        'glow': 'glow 4s ease-in-out infinite',
+        "border-beam": "border-beam calc(var(--duration) * 1s) linear infinite",
       },
     },
   },
