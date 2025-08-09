7972edbffdb66c7bc1427ece724baa3252e8c5b6
Garvit Kumar <garvitkajot854@gmail.com>
2025-08-09 09:46:33 +0000

    i think the devices not detecting that website is also a pwa because the

--- src/next.config.ts
+++ src/next.config.ts
@@ -7,7 +7,7 @@ const withPWA = withPWAInit({
   dest: 'public',
   sw: 'sw.js',
   register: true,
-  skipWaiting: true,
+  skipWaiting: false,
   disable: process.env.NODE_ENV === 'development',
   fallbacks: {
     document: '/_offline',
